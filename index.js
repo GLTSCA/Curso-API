cc28f441b9c1dbbdb2ab1f7bc377d344e8655237
GLTSCA <gltsca2004@gmail.com>
2025-07-25 18:37:04 -0300

    feat: implement basic arithmetic API operations and refactor index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require('express'); // Importing express
+const express = require("express"); // Importing express
+const { apiSomar, apiSubtrair, apiMultiplicar, apiDividir } = require('./api/operacoes')
+
 const app = express(); // Creating an instance of express
-const porta=3000; // Defining the port number
+const porta = 3000; // Defining the port number
+app.use(express.json()); // Middleware to parse JSON bodies
 
-app.get('/', (req, res) => {
-    req.query.num
-    res.send('This sum is: ' + (Number(req.query.num) + Number(req.query.num2))); // Sending a response for the root route
-});
 
-app.post('/', (req, res) => {
-    req.query.num
-    res.send('Was post'); // Sending a response for the root route
-});
 
+
+app.post("/somar", apiSomar);
+
+app.post("/subtrair", apiSubtrair);
+
+app.post("/multiplicar", apiMultiplicar);
+
+app.post("/dividir", apiDividir);
 
 app.listen(porta, () => {
-    console.log('Server is running on port ' + porta); // Logging when the server starts
+  console.log("Server is running on port " + porta); // Logging when the server starts
 }); // Listening on port 3000
---
+++ services/operacoes.js
@@ -0,0 +1,23 @@
+function somar(num1, num2){
+    return num1 + num2; // Function to sum two numbers
+}
+
+function subtrair(num1, num2){
+    return num1 - num2; 
+}
+
+
+function multiplicar(num1, num2){
+    return num1 * num2; 
+}
+
+function dividir(num1, num2){
+    return num1 / num2; 
+}
+
+
+
+
+
+
+module.exports = { somar, subtrair, multiplicar, dividir }  // Exporting the Somar function for use in other files
