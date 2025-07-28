8b2d95fadae7d3b234faaade4fcd45fef4845e5e
GLTSCA <gltsca2004@gmail.com>
2025-07-28 13:32:54 -0300

    feat: add error handling for arithmetic operations and create launch configuration

--- services/operacoes.js
+++ services/operacoes.js
@@ -1,17 +1,32 @@
 function somar(num1, num2){
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Invalid input: Both inputs must be numbers.");
+    }
     return num1 + num2; // Function to sum two numbers
 }
 
 function subtrair(num1, num2){
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Invalid input: Both inputs must be numbers.");
+    }
     return num1 - num2; 
 }
 
 
 function multiplicar(num1, num2){
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Invalid input: Both inputs must be numbers.");
+    }
     return num1 * num2; 
 }
 
 function dividir(num1, num2){
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Invalid input: Both inputs must be numbers.");
+    }
+    if (num2 === 0) {
+        throw new Error("Division by zero is not allowed.");
+    }
     return num1 / num2; 
 }
 
